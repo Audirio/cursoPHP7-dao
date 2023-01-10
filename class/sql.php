8600d5ba795e7bd95f33dc82af606abbd08df65b
Audirio <audiriobezerra@gmail.com>
2023-01-10 09:16:48 -0300

    criamos a classe usuarios

--- class/sql.php
+++ class/sql.php
@@ -1,23 +1,33 @@
 <?php
 
 class Sql extends PDO {
+
     private $conn;
 
     public function __construct(){
+
         $this->conn = new PDO("mysql:dbname=dbphp7;host=localhost;port=3308","root","");
+
     }
 
-    private function setParams($statment, $parameters = array()){
+    private function setParams($statement, $parameters = array()){
+
         foreach ($parameters as $key => $value) {
-            $this->setParams($key, $value);
+
+            $this->setParam($statement, $key, $value);
+
         }
+
     }
 
-    private function setParam($statment, $key, $value){
-        $statment->bindParam($key, $value);
+    private function setParam($statement, $key, $value){
+
+        $statement->bindParam($key, $value);
+
     }
 
     public function query1($rawQuery, $params = array()){
+
         $stmt = $this->conn->prepare($rawQuery);
 
         $this->setParams($stmt, $params);
--- class/usuario.php
+++ class/usuario.php
@@ -43,7 +43,28 @@ class Usuario {
 
         $sql = new Sql();
 
-        $resusts = $sql->select("SELECT * FROM tb_")
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
+            ":ID"=>$id
+        ));
+
+        if (count($results) > 0) {
+            $row = $results[0];
+
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+        }
+    }
+
+    public function __toString()
+    {
+        return json_encode(array(
+            "idusuario"=>$this->getIdusuario(),
+            "deslogin"=>$this->getDeslogin(),
+            "dessenha"=>$this->getDessenha(),
+            "dtcadastro"=>$this->getDtcadastro()->format("d/~m/Y H:i:s")
+        ));
     }
 }
 
