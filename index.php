60e0b9741d35a5d17a7485a5c29faa8c6d976251
Audirio <audiriobezerra@gmail.com>
2023-01-11 08:37:20 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,25 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//Carrega um usuário
+// $root = new Usuario();
+// $root->loadById((3));
+// echo $root;
 
-$root->loadById((3));
+//Carrega lista de usuários
+// $lista = Usuario::getList();
+// echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista de usuários buscando pelo login
+// $search = Usuario::search("ro");
+// echo json_encode($search);
+
+//Carrega um usuário usando o login e senha
+$usuario = new Usuario();
+$usuario->login("root", "1234");
+
+echo $usuario;
+
+
 
 ?>
