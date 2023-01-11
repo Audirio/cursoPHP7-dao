077c6ef9af08a97259c44b37628ef20b3f4ea3a6
Audirio <audiriobezerra@gmail.com>
2023-01-11 10:31:18 -0300

    metodo insert e update

--- index.php
+++ index.php
@@ -16,11 +16,23 @@ require_once("config.php");
 // echo json_encode($search);
 
 //Carrega um usuário usando o login e senha
+// $usuario = new Usuario();
+// $usuario->login("root", "1234");
+// echo $usuario;
+
+//Insert de um usuário novo
+// $aluno = new Usuario("aluno", "@lun0");
+// $aluno->insert();
+// echo $aluno;
+
+//Update de usuário
 $usuario = new Usuario();
-$usuario->login("root", "1234");
+
+$usuario->loadById(8);
+
+$usuario->update("professor", "65432");
 
 echo $usuario;
-
 
 
 ?>
